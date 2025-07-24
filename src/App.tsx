852f6380acba08a7f861843890652695d6c5a041
Herminio Garcia <hermano360@gmail.com>
2025-07-23 18:01:38 -0700

    Fix basename on React Router

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/listmap-web/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
